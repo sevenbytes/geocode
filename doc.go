e722f1494b05b2378b1f8601b6001ba71000ef72
Jakub Jarosz <jakub@jarosz.dev>
2022-10-25 09:33:51 +0100

    Initial commit

---
+++ doc.go
@@ -0,0 +1,3 @@
+// Package geocode provides primitives for geolocation searches.
+// It is a client library for Geolocation Services API by ESRI.
+package geocode
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sevenbytes/geocode
+
+go 1.19
